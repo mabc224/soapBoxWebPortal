e15ab09a6aed77e8fd56a8a37610fb27aee2f09e
mabc224 <mabc224@gmail.com>
2018-10-27 03:08:52 +0500

    implemented ag-grid with raw data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { AgGridModule } from 'ag-grid-angular';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    DashboardComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [AppComponent, DashboardComponent],
+    imports: [BrowserModule, AppRoutingModule, AgGridModule.withComponents([])],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -6,7 +6,12 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
+    // tslint:disable-next-line:max-line-length
+    public columnDefs: any = [{ headerName: 'Make', field: 'make' }, { headerName: 'Model', field: 'model' }, { headerName: 'Price', field: 'price' }];
+    // tslint:disable-next-line:max-line-length
+    public rowData: any = [{ make: 'Toyota', model: 'Celica', price: 35000 }, { make: 'Ford', model: 'Mondeo', price: 32000 }, { make: 'Porsche', model: 'Boxter', price: 72000 }];
     public title = 'Soap Box - Web Portal';
+
     constructor() {}
 
     ngOnInit() {}
