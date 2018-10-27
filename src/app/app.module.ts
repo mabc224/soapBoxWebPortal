8df3adc3f2c0cbc722fb83742c19f4579ea1db8c
mabc224 <mabc224@gmail.com>
2018-10-27 08:50:36 +0500

    angular material added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -7,10 +8,12 @@ import { CustomerComponent } from './components/dashboard/customer.component';
 import { AgGridModule } from 'ag-grid-angular';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { MatInputModule, MatFormFieldModule } from '@angular/material';
 
 @NgModule({
     declarations: [AppComponent, CustomerComponent],
-    imports: [BrowserModule, HttpClientModule, FormsModule, AppRoutingModule, AgGridModule.withComponents([])],
+    // tslint:disable-next-line:max-line-length
+    imports: [BrowserModule, BrowserAnimationsModule, HttpClientModule, FormsModule, AppRoutingModule, AgGridModule.withComponents([]), MatInputModule, MatFormFieldModule],
     providers: [],
     bootstrap: [AppComponent]
 })
